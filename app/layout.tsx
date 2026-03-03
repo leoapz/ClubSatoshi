043f9773400bc1358ac912d2dd3db2c88f0ae6ca
Leo <leoapaza2@gmail.com>
2026-03-03 20:37:40 -0300

    Change icon page

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,11 @@ export const metadata: Metadata = {
   title: "Club Satoshi | Comunidad Bitcoiner",
   description:
     "Aprende cómo Bitcoin impulsa una economía circular y sostenible.",
+  icons: {
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
+  },
 };
 
 export default function RootLayout({
